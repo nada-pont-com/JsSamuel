5a6a85e210e5fa79f79d4beaa96258aa32956f8d
Your Name <murillo_costa@estudante.sc.senai.br>
2018-11-12 15:48:45 -0200

    tempo f2

--- js/fase2.js
+++ js/fase2.js
@@ -2,6 +2,14 @@ import Player from './player.js';
 import Coin from './coin.js';
 import Tempo from "./tempo.js";
 import PlatForms from "./platForms.js";
+
+
+class fase2 extends Phaser.Scene{
+
+    constructor(){
+        super({key: "fase2"});
+        this.bat;
+        this.bg;
         this.obs;
         this.cursors;
         this.score = 0;
@@ -11,13 +19,6 @@ import PlatForms from "./platForms.js";
         this.player;
         this.w;
         this.h;
-import Tempo from "./tempo.js";
-class fase2 extends Phaser.Scene{
-
-    constructor(){
-        super({key: "fase2"});
-        this.bat;
-        this.bg;
         this.platforms;
         this.pontos = 0;
     }
@@ -60,9 +61,7 @@ class fase2 extends Phaser.Scene{
         this.platforms.criaObstaculos(2801, 144, 'limite');
 
         this.player = new Player(this);
-        this.player.create();
         this.coin = new Coin(this);
-        this.coin.create();
         //primeira
         this.coin.geraMoedas(200,250);
         this.coin.geraMoedas(230,250);
@@ -145,7 +144,6 @@ class fase2 extends Phaser.Scene{
         //fim moedas
         this.coin.criaTexto();
         this.tempo = new Tempo(this);
-        this.tempo.geraTempo();
         this.input.on("pointerdown",function(pointer){
             console.log("x: "+this.player.player.x);
             console.log("y: "+this.player.player.y);
@@ -157,8 +155,9 @@ class fase2 extends Phaser.Scene{
     }
 
     update(){
-        this.coin.update(this.player.player[]);
+        this.coin.update(this.player.player);
         this.player.update(this.coin.coins());
+        this.tempo.moveTempo(this.player.player);
         if (this.gameOver){
             return;
         }
