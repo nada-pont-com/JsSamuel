61740f8015dba7bd9dca6adeb642613939584365
nada-pont-com <nada.pont.com@gmail.com>
2018-11-14 13:57:56 -0200

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,29 @@
+
 class Menu{
     constructor(scene){
+
+        this.scene = scene.scene;
         this.add = scene.add;
         this.menu;
         this.input = scene.input;
         this.create();
     }
     create(){
-        this.menu = this.add.image(670,35,'menu');
+        this.menu = this.add.image(670,35,'pausa');
         this.menu.setInteractive();
         this.input.on("gameobjectdown",function(pointer,gameObject){
-            
+            this.scene.start("menu");
         })
     }   
 }
+class menu extends Phaser.Scene{
+    create(){
+        let play = this.add.image("play");
+        let reset = this.add.image("reset");
+        // let play = this.add.image("start");
+        let menu = this.add.image("menu");
+        this.input.on("gameobjectdown")
+    }
+}
+export default menu;
 export default Menu;
