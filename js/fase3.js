2dfa42bb58b910416b26e041a25f5b2dba9ed32e
DarkAlligator <willian_pereira@estudante.sc.senai.br>
2018-11-14 16:30:47 -0200

    tudo

--- js/fase3.js
+++ js/fase3.js
@@ -16,9 +16,7 @@ class fase3 extends Phaser.Scene{
     preload (){
         this.load.image('bgf3', 'assets/background/fase 3.png');//700x288
         this.load.image('ground', 'assets/obj/plataforma.png');//700x60
-        this.load.image('plat1','assets/obj/dog house.png');//faixada da loja azul
-        this.load.image('plat3','assets/obj/omega shop.png');//faixada da loja verde
-        this.load.image('plat2', 'assets/obj/plat2.png');//banquinho
+        this.load.image('tronco','assets/obj/tronco2.png');//tronco        
         this.load.image('coin', 'assets/obj/coin.png');//22x22
         this.load.spritesheet('dude', 'assets/skins/dude.png', { frameWidth: 31, frameHeight: 36 });//32x48
         this.load.spritesheet('bat', 'assets/mobs/bat.png', {frameWidth: 31, frameHeight: 36});//32x48
@@ -37,10 +35,8 @@ class fase3 extends Phaser.Scene{
         this.platforms = new PlatForms(this);
 
         this.platforms.create('ground');
-        this.platforms.criaObstaculos(574, 155, 'plat1',2);
-        this.platforms.criaObstaculos(344, 250, 'plat2',2);
-        this.platforms.criaObstaculos(1268, 155, 'plat3',2);
-        this.platforms.criaObstaculos(1000, 196, 'OBS7',2);
+        this.platforms.criaObstaculos(574, 200, 'tronco',1);
+        this.platforms.criaObstaculos(1074, 200, 'tronco',1);
         this.platforms.criaObstaculos(-1, 144 , 'limite',1);
         this.platforms.criaObstaculos(2801, 144, 'limite',1);
         
@@ -48,28 +44,21 @@ class fase3 extends Phaser.Scene{
 
         this.player = new Player(this);
         this.coin = new Coin(this);
-        this.coin.geraMoedas(896.5149120000165,15);
-        this.coin.geraMoedas(540.7958079999963,25);
-        this.coin.geraMoedas(995.6822400000013,160);
-        this.coin.geraMoedas(1240.7678079999814,100);
-        this.coin.geraMoedas(1940.5149120000165,15);
-        this.coin.geraMoedas(2296.7958079999963,25);
-        this.coin.geraMoedas(2395.6822400000013,160);
-        this.coin.geraMoedas(2751.6822400000013,160);
-        this.coin.geraMoedas(2640.7678079999814,100);
-        this.coin.geraMoedas(1234.5149120000165,15);
-        this.coin.geraMoedas(2800,25);
-        this.coin.geraMoedas(193,180);
-        this.coin.geraMoedas(322,10);
-        this.coin.geraMoedas(444.5149120000165,190);
-        this.coin.geraMoedas(777.7958079999963,25);
-        this.coin.geraMoedas(369.6822400000013,160);
-        this.coin.geraMoedas(246.7678079999814,100);
-        this.coin.geraMoedas(1689,250);
-        this.coin.geraMoedas(1432.7958079999963,25);
-        this.coin.geraMoedas(640,160);
-        this.coin.geraMoedas(2000.7678079999814,100);
-        this.coin.geraMoedas(2200.7958079999963,25);
+        this.coin.geraMoedas(200,250);
+        this.coin.geraMoedas(230,250);
+        this.coin.geraMoedas(260,250);
+        this.coin.geraMoedas(290,250);
+        this.coin.geraMoedas(320,250);
+        this.coin.geraMoedas(350,250);
+        this.coin.geraMoedas(380,250);
+        this.coin.geraMoedas(410,210);
+        this.coin.geraMoedas(440,180);
+        this.coin.geraMoedas(470,180);
+        this.coin.geraMoedas(500,180);
+        this.coin.geraMoedas(530,180);
+        this.coin.geraMoedas(560,180);
+        this.coin.geraMoedas(590,180);
+        
         this.coin.criaTexto();
         this.tempo = new Tempo(this);
 
